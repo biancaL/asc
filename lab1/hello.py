0dec9c3e61959915e8f1c94fed3e4f4ae810a7a7
Bianca Leuca <bianca.leuca@mozaicworks.com>
2016-02-26 08:17:21 +0200

    Lab structure

---
+++ lab1/hello.py
@@ -0,0 +1,23 @@
+"""
+Task 1 - Let's print the current time!
+
+Objectives:
+- use documentation!!!
+- print
+- print with %
+- print without new line
+- format
+
+Print a few messages (e.g. Python is cool!).
+Print the current date and time. Also print it using % and using format().
+---> see examples from 'Stringuri' section.
+"""
+
+
+# Hints for date and time: datetime module for objects representing time,
+# strftime function for formatting
+# https://docs.python.org/2/library/datetime.html#datetime-objects
+# https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
+
+# Use only the official Python documentation to solve this exercise.
+
---
+++ lab1/task3.py
@@ -0,0 +1,44 @@
+"""
+Objectives:
+- Lists
+- Dict
+- constructs: loops, conditionals
+- more work with files
+- functions
+"""
+
+#TODO implement the following functions
+
+# function #1
+"""
+    Returns the number of lines in a file given as parameter.
+    @param filename: the file's name
+"""
+
+# function #2
+"""
+    Reads the content of a file and fills the given list with the sentences
+    found in the file
+    @param filename: the file's name
+    @param sentences: the list that will be contain the sentences
+"""
+    
+# function #3
+"""
+    Return a list of the top N most used words in a given file
+    @param filename: the file's name
+    @param n: the number of words in the top, default is 5
+"""
+#bonus 1p: implement your own sort method instead of using an existing one
+
+
+
+if __name__ == "__main__":
+    
+    filename = "text"    
+
+    # TODO test the functions
+
+    # TODO print all the sentences with less than 20 words
+
+    # TODO write the most used words in a file, one per line
